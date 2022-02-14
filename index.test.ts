e5718a0c34cfcabd0a6ba7d42d550b35866b67a3
Filip Tammergård <filip@tammergard.se>
2022-02-14 14:23:01 +0100

    feat: update readme

--- index.test.ts
+++ index.test.ts
@@ -6,6 +6,9 @@ import {
   getReadingTime,
 } from "./index"
 
+const TEXT =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
 describe("countCharacters", () => {
   it("handles empty string", () => {
     assert.equal(countCharacters(""), 0)
@@ -38,47 +41,52 @@ describe("countSequenceOccurances", () => {
     assert.equal(countSequenceOccurances("", "text"), 0)
   })
   it("handles non-empty string", () => {
-    assert.equal(countSequenceOccurances("text text characters", "text"), 2)
+    assert.equal(countSequenceOccurances(TEXT, "dolor"), 4)
   })
   it("handles case insensitive counting", () => {
-    assert.equal(countSequenceOccurances("Text teXt", "teXt"), 2)
+    assert.equal(countSequenceOccurances(TEXT, "doLOR"), 4)
   })
   it("handles case sensitive counting", () => {
     assert.equal(
-      countSequenceOccurances("Text teXt", "teXt", {
+      countSequenceOccurances(TEXT, "doLOR", {
         caseSensitivity: "sensitive",
       }),
-      1,
+      0,
     )
   })
 })
 
 describe("getReadingTime", () => {
-  const text =
-    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente ut nulla earum inventore possimus quas ab, omnis beatae architecto quisquam!"
   it("handles empty string", () => {
     assert.equal(getReadingTime("").words, 0)
     assert.equal(getReadingTime("").minutes, 0)
     assert.equal(getReadingTime("").milliseconds, 0)
   })
   it("handles non-empty string", () => {
-    assert.equal(getReadingTime(text).words, 20)
-    assert.equal(getReadingTime(text).minutes, 0.1)
-    assert.equal(getReadingTime(text).milliseconds, 6000)
+    assert.equal(getReadingTime(TEXT).words, 69)
+    assert.equal(getReadingTime(TEXT).minutes, 0.345)
+    assert.equal(getReadingTime(TEXT).milliseconds, 20700)
   })
   it("handles white space", () => {
-    const textWithWhiteSpace = ` \t  ${text}\n `
-    assert.deepEqual(getReadingTime(text), getReadingTime(textWithWhiteSpace))
+    const textWithWhiteSpace = ` \t  ${TEXT}\n `
+    assert.deepEqual(getReadingTime(TEXT), getReadingTime(textWithWhiteSpace))
   })
   it("handles custom reading speed", () => {
-    const WORDS_PER_MINUTE = 100
     assert.notEqual(
-      getReadingTime(text),
-      getReadingTime(text, { wordsPerMinute: WORDS_PER_MINUTE }),
+      getReadingTime(TEXT),
+      getReadingTime(TEXT, { wordsPerMinute: 100 }),
     )
     assert.equal(
-      getReadingTime(text, { wordsPerMinute: 1 }).milliseconds,
-      2 * getReadingTime(text, { wordsPerMinute: 2 }).milliseconds,
+      getReadingTime(TEXT, { wordsPerMinute: 1 }).words,
+      getReadingTime(TEXT, { wordsPerMinute: 2 }).words,
+    )
+    assert.equal(
+      getReadingTime(TEXT, { wordsPerMinute: 1 }).minutes,
+      2 * getReadingTime(TEXT, { wordsPerMinute: 2 }).minutes,
+    )
+    assert.equal(
+      getReadingTime(TEXT, { wordsPerMinute: 1 }).milliseconds,
+      2 * getReadingTime(TEXT, { wordsPerMinute: 2 }).milliseconds,
     )
   })
 })
